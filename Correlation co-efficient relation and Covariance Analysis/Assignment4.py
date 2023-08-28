4578d864f52e99d32b13a239f5db20abcd67f9da
Sushree Sasmita <143263248+sushreesasmitanayak@users.noreply.github.com>
2023-08-28 19:35:18 +0530

    Create Assignment4.py

---
+++ Correlation co-efficient relation and Covariance Analysis/Assignment4.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sbs
+plt.style.use('seaborn-whitegrid')
+
+df=pd.read_csv("Downloads/student-mat.csv")
+
+df.head()
+
+df.info()
+
+dfn=df[['traveltime','studytime']]
+
+dfn.head()
+
+x=dfn['traveltime']
+y=dfn['studytime']
+sbs.lineplot(dfn)
+plt.show()
+
+from scipy.stats import norm
+sbs.lineplot(x=dfn['traveltime'],y=dfn['studytime'],dashes=True)
+
+corelation=dfn.corr()
+print(corelation)
+
+sbs.heatmap(corelation,cmap ="YlGnBu")
+plt.show()
+
+covar=dfn.cov()
+print(covar)
+
+sbs.heatmap(covar,cmap='YlGnBu')
+plt.show()
